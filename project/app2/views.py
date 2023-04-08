17d42fa8e923b5f632c7c1748e9ba6d5666c9ec0
Swagato Chakraborty <swagatochakraborty123@gmail.com>
2023-04-08 19:39:12 +0530

    Second Commit

--- project/app2/views.py
+++ project/app2/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def func2(request):
-    return HttpResponse("Hello World 2")
+    # return HttpResponse("Hello World 2")
+    return render(request, 'file_1.html')
